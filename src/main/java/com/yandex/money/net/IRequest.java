f8c91d62b4abe6876d996a18f0d2ec3481e95b20
mdv <melnikovdv@gmail.com>
2014-04-09 16:56:21 +0400

    URI_API set to production

--- src/main/java/com/yandex/money/net/IRequest.java
+++ src/main/java/com/yandex/money/net/IRequest.java
@@ -11,9 +11,7 @@ import java.net.URL;
  */
 public interface IRequest<T> {
 
-//    String URI_API = "https://money.yandex.ru/api";
-//    String URI_API = "https://baku.yandex.ru/api";
-    String URI_API = "https://polo.yandex.ru/api";
+    String URI_API = "https://money.yandex.ru/api";
 
     URL requestURL() throws MalformedURLException;
 
