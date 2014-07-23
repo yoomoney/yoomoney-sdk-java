4d54f5397e0ae49469c7ada38711da5ab49b0908
Slava Yasevich <vyasevich@yamoney.ru>
2014-07-23 20:51:44 +0400

    BaseProcessPayment is set to abstract.

--- src/main/java/com/yandex/money/model/cps/BaseProcessPayment.java
+++ src/main/java/com/yandex/money/model/cps/BaseProcessPayment.java
@@ -7,7 +7,7 @@ import java.util.Map;
 /**
  * @author Slava Yasevich (vyasevich@yamoney.ru)
  */
-public class BaseProcessPayment implements MethodResponse {
+public abstract class BaseProcessPayment implements MethodResponse {
 
     protected static final String MEMBER_STATUS = "status";
     protected static final String MEMBER_ERROR = "error";
