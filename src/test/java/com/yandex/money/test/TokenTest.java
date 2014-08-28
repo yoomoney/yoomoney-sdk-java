b8def041e1b7cd76c40864cfcbed29d0965c007a
Slava Yasevich <vyasevich@yamoney.ru>
2014-08-28 18:56:28 +0400

    Implemented AuxToken. NOT TESTED.

--- src/test/java/com/yandex/money/test/TokenTest.java
+++ src/test/java/com/yandex/money/test/TokenTest.java
@@ -3,7 +3,10 @@ package com.yandex.money.test;
 import com.yandex.money.api.exceptions.InsufficientScopeException;
 import com.yandex.money.api.exceptions.InvalidRequestException;
 import com.yandex.money.api.exceptions.InvalidTokenException;
+import com.yandex.money.api.methods.AccountInfo;
+import com.yandex.money.api.methods.AuxToken;
 import com.yandex.money.api.methods.Token;
+import com.yandex.money.api.model.Scope;
 import com.yandex.money.api.net.DefaultApiClient;
 import com.yandex.money.api.net.OAuth2Session;
 
@@ -11,6 +14,8 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * @author Slava Yasevich (vyasevich@yamoney.ru)
@@ -23,7 +28,7 @@ public class TokenTest implements ApiTest {
     public void testToken() throws InvalidTokenException, InsufficientScopeException,
             InvalidRequestException, IOException {
 
-        OAuth2Session session = new OAuth2Session(new DefaultApiClient(CLIENT_ID));
+        OAuth2Session session = new OAuth2Session(new DefaultApiClient(CLIENT_ID, true));
         session.setDebugLogging(true);
 
         Token token = session.execute(new Token.Request(CODE, CLIENT_ID, REDIRECT_URI, null));
@@ -31,5 +36,24 @@ public class TokenTest implements ApiTest {
 
         session.setAccessToken(token.getAccessToken());
         session.execute(new Token.Revoke());
+    }
+
+    @Test
+    public void testAuxToken() throws InvalidTokenException, InsufficientScopeException,
+            InvalidRequestException, IOException {
+
+        OAuth2Session session = new OAuth2Session(new DefaultApiClient(CLIENT_ID, true));
+        session.setDebugLogging(true);
+        session.setAccessToken(ACCESS_TOKEN);
+
+        Set<Scope> scopes = new HashSet<>();
+        scopes.add(Scope.ACCOUNT_INFO);
+
+        AuxToken auxToken = session.execute(new AuxToken.Request(scopes));
+        Assert.assertNotNull(auxToken.getAuxToken());
+
+        AccountInfo accountInfo = session.execute(new AccountInfo.Request());
+        Assert.assertNotNull(accountInfo.getAccount());
+        Assert.assertNotNull(accountInfo.getBalance());
     }
 }
