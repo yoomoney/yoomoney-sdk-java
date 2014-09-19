ccd6e47a80ccbe78de9c3d9c9b398b25b7551010
Slava Yasevich <vyasevich@yamoney.ru>
2014-09-19 19:03:34 +0400

    Added header Location.

--- src/main/java/com/yandex/money/api/utils/HttpHeaders.java
+++ src/main/java/com/yandex/money/api/utils/HttpHeaders.java
@@ -10,6 +10,7 @@ public final class HttpHeaders {
     public static final String AUTHORIZATION = "Authorization";
     public static final String CONTENT_LENGTH = "Content-Length";
     public static final String CONTENT_TYPE = "Content-Type";
+    public static final String LOCATION = "Location";
     public static final String USER_AGENT = "User-Agent";
     public static final String WWW_AUTHENTICATE = "WWW-Authenticate";
 }
