b9d773043001d05d13feac159b40625512504d18
Slava Yasevich <vyasevich@yamoney.ru>
2014-12-11 18:29:23 +0300

    Introduced DECIMAL pattern.

--- src/main/java/com/yandex/money/api/utils/Patterns.java
+++ src/main/java/com/yandex/money/api/utils/Patterns.java
@@ -3,7 +3,7 @@ package com.yandex.money.api.utils;
 /**
  * Common patterns.
  *
- * @author Slava Yasevich (yasevich.slava@gmail.com)
+ * @author Slava Yasevich (vyasevich@yamoney.ru)
  */
 public final class Patterns {
 
@@ -28,6 +28,11 @@ public final class Patterns {
      */
     public static final String EMAIL = "[a-zA-Z0-9\\+\\._%\\-\\+]{1,256}@[a-zA-Z0-9]" +
             "[a-zA-Z0-9\\-]{0,64}(\\.[a-zA-Z0-9][a-zA-Z0-9\\-]{0,25})+";
+
+    /**
+     * Strings math this pattern are decimals.
+     */
+    public static final String DECIMAL = "[\\+\\-]?\\d*(\\.(\\d*)?)?";
 
     private Patterns() {
     }
