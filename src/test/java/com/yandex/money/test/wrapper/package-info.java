45e62cd1eefc9ed8551b6eeb604af5458cb88ff9
Ak Valeriy <mtakvel@gmail.com>
2016-06-14 11:48:36 +0300

    Сделать возможность смены URL в HostsProvider.java 2

--- src/test/java/com/yandex/money/test/wrapper/package-info.java
+++ src/test/java/com/yandex/money/test/wrapper/package-info.java
@@ -3,13 +3,11 @@
  *
  * Example:
  *
- *
  * DefaultApiClient apiClient = new CustomApiClient(myClientId,  "https://my.stub.com")
  *
- * String tokenRevokePath = "/token/revoke"
+ * OAuth2Session oAuth2Session = new OAuth2Session(apiClient);
  *
- *  OAuth2Session oAuth2Session = new OAuth2Session(apiClient);
- *  oAuth2Session.execute(new CustomPathRequestWrapper<>(new Token.Revoke(), tokenRevokePath);
+ * oAuth2Session.execute(new CustomPathRequestWrapper<>(new Token.Revoke(), "/token/revoke"));
  *
  */
 package com.yandex.money.test.wrapper;
