d6e36b3e767962abb009ec3d0cff88f7b9c0141a
irshabaev <irshabaev@yamoney.ru>
2018-04-02 12:56:41 +0300

    Make "Undefined" final and singleton

--- src/main/java/com/yandex/money/api/model/showcase/components/Undefined.java
+++ src/main/java/com/yandex/money/api/model/showcase/components/Undefined.java
@@ -1,6 +1,15 @@
 package com.yandex.money.api.model.showcase.components;
 
-public class Undefined extends Component {
+public final class Undefined extends Component {
+
+    private static final Undefined INSTANCE = new Undefined();
+
+    private Undefined() {
+    }
+
+    public static Component getInstance() {
+        return INSTANCE;
+    }
 
     @Override
     public boolean isValid() {
