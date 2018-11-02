192485cf3ac89169d4b6e359965cf896019108d0
irshabaev <irshabaev@yamoney.ru>
2018-11-02 11:11:25 +0300

    DRO-2878: Made SpendingCategory final

--- src/main/java/com/yandex/money/api/model/SpendingCategory.java
+++ src/main/java/com/yandex/money/api/model/SpendingCategory.java
@@ -6,7 +6,7 @@ import java.math.BigDecimal;
 
 import static com.yandex.money.api.util.Common.checkNotNull;
 
-public class SpendingCategory {
+public final class SpendingCategory {
 
     /**
      * Name of the category
